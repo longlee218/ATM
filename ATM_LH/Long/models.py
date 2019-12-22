ac091e3979941d1465363e093a88809cba7ab6cd
hoang <hoangbui1699@gmail.com>
2019-12-23 02:07:08 +0700

    sửa models thành tiếng anh

--- ATM_LH/Long/models.py
+++ ATM_LH/Long/models.py
@@ -1,47 +1,50 @@
 from django.db import models
-from ATM_LH.Hoang.models import TinhThanh
-# Create your models here.
 
 
-class NganHang(models.Model):
-    ma_ngan_hang = models.CharField(primary_key=True, max_length=8)
-    ten_ngan_hang = models.TextField()
-    tru_so = models.TextField()
-    tong_dai = models.CharField(max_length=10)
-    chi_nhanh = models.ManyToManyField(TinhThanh, through="ChiNhanh")
+class Province(models.Model):
+    province_id = models.CharField(primary_key=True, max_length=2)
+    province_name = models.TextField()
 
 
-class ChiNhanh(models.Model):
-    ma_tinh_thanh = models.ForeignKey(TinhThanh, on_delete=models.CASCADE)
-    ma_ngan_hang = models.ForeignKey(NganHang, on_delete=models.CASCADE)
-    ma_chi_nhanh = models.CharField(max_length=5, primary_key=True)
-    ten_chi_nhanh = models.TextField()
-    dia_chi = models.TextField()
+class Bank(models.Model):
+    bank_id = models.CharField(primary_key=True, max_length=8)
+    bank_name = models.TextField()
+    bank_headquarters = models.TextField()
+    bank_phone_number = models.CharField(max_length=10)
+    branches = models.ManyToManyField(Province, through="Branch")
 
 
-class NhanVien(models.Model):
-    ma_chi_nhanh = models.ForeignKey(ChiNhanh, on_delete=models.CASCADE)
-    ma_nhan_vien = models.CharField(max_length=3, primary_key=True)
-    ho_ten_nv = models.CharField(max_length=32)
-    ten_dang_nhap = models.CharField(max_length=18)
-    mat_khau = models.TextField()
+class Branch(models.Model):
+    province_id = models.ForeignKey(Province, on_delete=models.CASCADE)
+    bank_id = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    branch_id = models.CharField(max_length=5, primary_key=True)
+    branch_name = models.CharField(max_length=100)
+    branch_address = models.TextField()
 
 
-class CayATM(models.Model):
-    TRANG_THAI = (
-        ('1', 'Hoat dong'),
-        ('2', 'Bao tri'),
-        ('3', 'Hong'),
+class Employee(models.Model):
+    branch_id = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    employee_id = models.CharField(max_length=5, primary_key=True)
+    full_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=32)
+    emp_password = models.CharField(max_length=50)
+
+
+class ATM(models.Model):
+    choice = (
+        ('1', 'Active'),
+        ('0', 'Deactive'),
     )
-    ma_nhan_vien = models.OneToOneField(NhanVien, on_delete=models.CASCADE)
-    ma_cay = models.CharField(max_length=5, primary_key=True)
-    dia_diem = models.TextField()
-    so_du = models.BigIntegerField()
-    trang_thai = models.CharField(max_length=1, choices=TRANG_THAI)
+    employee_id = models.OneToOneField(Employee, on_delete=models.CASCADE)
+    atm_id = models.CharField(max_length=5, primary_key=True)
+    address = models.TextField()
+    atm_balance = models.BigIntegerField()
+    status = models.CharField(max_length=1, choices=choice)
 
 
-class LichSuDoTien(models.Model):
-    ma_do_tien = models.CharField(max_length=3, primary_key=True)
-    ma_cay = models.ForeignKey(CayATM, on_delete=models.CASCADE)
-    thoi_gian = models.DateTimeField()
-    so_tien_do = models.BigIntegerField()
+class HistoryMoney(models.Model):
+    history_id = models.CharField(max_length=3, primary_key=True)
+    atm_id = models.ForeignKey(ATM, on_delete=models.CASCADE)
+    history_time = models.DateTimeField()
+    money = models.BigIntegerField()
+
