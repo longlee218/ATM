304f00e4e059787cfcff242f2441dda376b7f869
hoang <hoangbui1699@gmail.com>
2020-01-13 00:39:20 +0700

    commit to hoang branch 13/1/2020

--- ATM_LH/Long/forms.py
+++ ATM_LH/Long/forms.py
@@ -1,11 +1,2 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from Hoang.models import Customer
 
 
-class SignUp(forms.ModelForm):
-    class Meta:
-        model = Customer
-        fields = ('branch_id', 'full_name', 'birthday', 'gender', 'hometown', 'phone_number',
-                  'email', 'card_type', 'card_no',)
